ceea4694c40fe6df2e8384a6a0ebeefac6405c8b
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-07-15 16:12:20 +0100

    Grid will be setup appropriately

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,9 +1,32 @@
 class Grid
 
-
-
-	def count
-		100
+	def initialize
+		@home_grid = setup_grid
 	end
 
+	def home_grid
+		@home_grid
+	end
+
+	def count
+		@home_grid.count
+	end
+
+	def setup_grid
+		generic_grid = []
+
+		make_coordinates(generic_grid)
+
+		return generic_grid
+	end
+
+	def make_coordinates(generic_grid)
+		letters = ('A'..'J').to_a
+
+		letters.each do |letter|
+			1.upto(letters.length) do |current_number|
+				generic_grid << {"#{letter}#{current_number}" => Cell.new}
+			end
+		end
+	end
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,15 +1,24 @@
 require 'grid'
 
-describe Grid do 
+describe Grid do
 
-	it 'has 100 cells' do
-		grid = Grid.new
+	let(:grid) { Grid.new }
+	let(:a_cell) { double :cell }
+
+	it 'the grid is an array of hashes' do
+		expect(grid.home_grid.class).to eq Array
+	end
+
+	xit 'will have a cell called A1' do
+		expect(grid.home_grid).to include 'A1' => a_cell
+	end
+
+	it 'will have 100 cells' do
 		expect(grid.count).to eq 100
 	end
 
-	# it 'the cells in the grid to have co-ordinates' do
-	# 	grid = Grid.new
-	# 	expect(grid).to contain('A4', 'E7', 'D2')
-	# end
+	xit 'Each elemnet of the grid has cell' do
+		expect(grid.home_grid).to include 'B6' => a_cell
+	end
 
 end
